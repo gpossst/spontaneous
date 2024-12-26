1da87cb9ce3a258fef64981435948d16d0b67951
gpostVT <gep100iosgames@gmail.com>
2024-12-25 21:50:51 -0500

    fix: unnecessarily large table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import ResortTable from "./components/ResortTable";
 export default function Home() {
   return (
-    <div className="min-h-screen w-full font-[family-name:var(--font-geist-sans)]">
+    <div className=" w-full font-[family-name:var(--font-geist-sans)]">
       <div className="h-full w-full px-4">
         <ResortTable />
       </div>
