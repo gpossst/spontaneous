d8107ece63795b4eb1eee70e94d7b17f435f089e
gpostVT <gep100iosgames@gmail.com>
2025-01-01 15:06:17 -0500

    feat: add logs page

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -3,15 +3,27 @@ import Link from "next/link";
 import Logo from "./Logo";
 export default function HomeNav() {
   return (
-    <div className="flex w-full justify-between items-center pt-4 px-4">
-      <Logo />
-      <h1 className="text-3xl font-pacifico font-bold text-sky-500">Lifty</h1>
-      <Link
-        className="text-lg border-2 font-roboto text-sky-500 border-sky-500 rounded-lg px-4 py-2 hover:bg-sky-300 hover:border-sky-300 hover:text-white transition-colors"
-        href="/vote"
-      >
-        Vote
-      </Link>
+    <div className="flex w-full items-center pt-4 px-4">
+      <div className="flex-1">
+        <Logo />
+      </div>
+      <h1 className="flex-1 text-center text-3xl font-pacifico font-bold text-sky-500">
+        Lifty
+      </h1>
+      <div className="flex gap-4 flex-1 justify-end">
+        <Link
+          className="text-lg font-roboto text-sky-500 rounded-lg px-4 py-2"
+          href="/logs"
+        >
+          Logs
+        </Link>
+        <Link
+          className="text-lg border-2 font-roboto text-sky-500 border-sky-500 rounded-lg px-4 py-2 hover:bg-sky-300 hover:border-sky-300 hover:text-white transition-colors"
+          href="/vote"
+        >
+          Vote
+        </Link>
+      </div>
     </div>
   );
 }
